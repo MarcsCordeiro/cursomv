be73ef2aabee7f91e4d88b6706a352637f3bb018
Marcos Cordeiro <vicentevieira300@gmail.com>
2018-07-20 23:43:04 -0300

    Criando operacao de instaciacao

--- src/main/java/com/marcos/cursomv/CursomvApplication.java
+++ src/main/java/com/marcos/cursomv/CursomvApplication.java
@@ -1,12 +1,30 @@
 package com.marcos.cursomv;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomvApplication {
+import com.marcos.cursomv.domain.Categoria;
+import com.marcos.cursomv.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomvApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomvApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
