9ab3cde0635b663b59f08d7c24579f593561a936
Marcos Cordeiro <vicentevieira300@gmail.com>
2018-08-01 23:02:24 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/marcos/cursomv/CursomvApplication.java
+++ src/main/java/com/marcos/cursomv/CursomvApplication.java
@@ -1,5 +1,6 @@
 package com.marcos.cursomv;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.marcos.cursomv.domain.Cidade;
 import com.marcos.cursomv.domain.Cliente;
 import com.marcos.cursomv.domain.Endereco;
 import com.marcos.cursomv.domain.Estado;
+import com.marcos.cursomv.domain.Pagamento;
+import com.marcos.cursomv.domain.PagamentoComBoleto;
+import com.marcos.cursomv.domain.PagamentoComCartao;
+import com.marcos.cursomv.domain.Pedido;
 import com.marcos.cursomv.domain.Produto;
+import com.marcos.cursomv.domain.enums.EstadoPagamento;
 import com.marcos.cursomv.domain.enums.TipoCliente;
 import com.marcos.cursomv.repositories.CategoriaRepository;
 import com.marcos.cursomv.repositories.CidadeRepository;
 import com.marcos.cursomv.repositories.ClienteRepository;
 import com.marcos.cursomv.repositories.EnderecoRepository;
 import com.marcos.cursomv.repositories.EstadoRepository;
+import com.marcos.cursomv.repositories.PagamentoRepository;
+import com.marcos.cursomv.repositories.PedidoRepository;
 import com.marcos.cursomv.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomvApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomvApplication.class, args);
@@ -85,7 +97,19 @@ public class CursomvApplication implements CommandLineRunner {
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
+		Pedido ped1 = new Pedido(null,sdf.parse( "30/09/2017 10:32"), e1, cli1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), e2, cli1);
 		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 }
---
+++ src/main/java/com/marcos/cursomv/domain/Pedido.java
@@ -0,0 +1,113 @@
+package com.marcos.cursomv.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Pedido implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private Date instate;
+	
+	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
+	private Pagamento pagamento;
+	
+	@ManyToOne
+	@JoinColumn(name="id_endereco_de_entrada_fk")
+	private Endereco enderecoDeEntrega;
+	
+	@ManyToOne
+	@JoinColumn(name="id_cliente_fk")
+	private Cliente cliente;
+	
+	public Pedido() {
+	}
+
+	public Pedido(Integer id, Date instate, Endereco enderecoDeEntrega, Cliente cliente) {
+		super();
+		this.id = id;
+		this.instate = instate;
+		this.enderecoDeEntrega = enderecoDeEntrega;
+		this.cliente = cliente;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getInstate() {
+		return instate;
+	}
+
+	public void setInstate(Date instate) {
+		this.instate = instate;
+	}
+
+	public Pagamento getPagamento() {
+		return pagamento;
+	}
+
+	public void setPagamento(Pagamento pagamento) {
+		this.pagamento = pagamento;
+	}
+
+	public Endereco getEnderecoDeEntrega() {
+		return enderecoDeEntrega;
+	}
+
+	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
+		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
